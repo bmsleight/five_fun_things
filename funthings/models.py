05c1a1fad3fbac52e10551832bc7e95a5398188e
Brendan Sleight <bms@barwap.com>
2020-04-19 14:58:01 +0000

    Working on model and rought structure

--- funthings/models.py
+++ funthings/models.py
@@ -1,3 +1,25 @@
 from django.db import models
+from django.conf import settings
+from datetime import date 
 
-# Create your models here.
+class Thing(models.Model):
+    funster = models.ForeignKey(
+        settings.AUTH_USER_MODEL,
+        on_delete=models.CASCADE,
+    )
+    thing = models.CharField(
+        max_length=160, 
+        help_text="Max 160 Chars."
+        )
+    thing_date = models.DateField(
+        default=date.today, 
+        verbose_name="Thing Date"
+        )
+    photo = models.ImageField(
+        upload_to ='uploads/%Y/%m/%d/', 
+        blank=True,editable=True,
+        verbose_name="Thing Photo"
+        )
+    
+    def __str__(self):
+        return self.description_text + " on " + self.thing_date.strftime("%Y-%m-%d")
