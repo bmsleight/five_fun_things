ce001112b1fae5e911b7c75446ec1a71b0b3f06a
Brendan Sleight <bms@barwap.com>
2020-04-30 19:47:20 +0000

    Apple add to homescreen

--- funthings/urls.py
+++ funthings/urls.py
@@ -1,7 +1,7 @@
 from django.urls import path
 from django.conf.urls import url
 from django.views.generic import TemplateView
-
+from django.contrib.auth.decorators import login_required
 
 from . import views
 
@@ -10,7 +10,7 @@ urlpatterns = [
     path('<int:year>/<int:month>/<int:day>/',
          views.five_fun_form, name='five_fun_form'),
     path('journal/', views.date_view),
-    path('cloud/', TemplateView.as_view(template_name='cloud.html')),
+    path('cloud/', login_required(TemplateView.as_view(template_name='cloud.html'))),
     path('cloud/cloud.png', views.cloud_view),
     path('', views.ThingList.as_view(), name='list'),
     ]
